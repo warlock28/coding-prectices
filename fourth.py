0568f8891222639195d7258c1c3b56fcbdd043af
warlock28 <nitin.kush72800@gmail.com>
2025-10-30 18:43:59 +0530

    add the some python code files

---
+++ fourth.py
@@ -0,0 +1,4 @@
+def my_function(name): # name is a parameter
+  print("Hello", name)
+
+my_function("Emil") # "Emil" is an argument
--- new.py
+++ new.py
@@ -3,3 +3,6 @@ b =23
 b = a+b
 print(b)
 print("Hello, World!")
+print("This is a new file.")
+print("The sum of a and b is:", b)
+print("Goodbye, World!")
