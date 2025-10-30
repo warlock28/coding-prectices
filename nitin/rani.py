671339538206e8d648f4b285e5dc33cce58ac556
warlock28 <nitin.kush72800@gmail.com>
2025-10-30 18:54:02 +0530

    this is first commit

---
+++ nitin/rani.py
@@ -0,0 +1,16 @@
+def changecase(n):
+  def changecase(func):
+    def myinner():
+      if n == 1:
+        a = func().lower()
+      else:
+        a = func().upper()
+      return a
+    return myinner
+  return changecase
+
+@changecase(1)
+def myfunction():
+  return "Hello Linus"
+
+print(myfunction())
