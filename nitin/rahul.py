2d0ff44e4e69aa6ebbb3ec56f4d3dd492c727976
warlock28 <nitin.kush72800@gmail.com>
2025-10-30 18:55:18 +0530

    there is third commit

---
+++ nitin/rahul.py
@@ -0,0 +1,10 @@
+def changecase(func):
+  def myinner(*args, **kwargs):
+    return func(*args, **kwargs).upper()
+  return myinner
+
+@changecase
+def myfunction(nam):
+  return "Hello " + nam
+
+print(myfunction("John"))
